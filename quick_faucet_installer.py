c17126c48b26d2276ced834f50e1639f4607c652
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-02-27 19:03:49 +0530

    Update quick_faucet_installer.py

--- quick_faucet_installer.py
+++ quick_faucet_installer.py
@@ -1,6 +1,6 @@
 import os
 def faucet_downloader():
-  os.system('sudo apt-get install curl gnupg apt-transport-https lsb-release
+  os.system('sudo apt-get install curl gnupg apt-transport-https lsb-release')
   os.system('echo "deb https://packagecloud.io/faucetsdn/faucet/$(lsb_release -si | awk \'{print tolower($0)}\')/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/faucet.list')
   os.system('curl -L https://packagecloud.io/faucetsdn/faucet/gpgkey | sudo apt-key add -')
   os.system('sudo apt-get update')

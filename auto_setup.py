1520c71c1dba264c69443071b31e2b4ea4d00fe5
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-02-28 11:06:26 +0530

    Update auto_setup.py

--- auto_setup.py
+++ auto_setup.py
@@ -1,4 +1,4 @@
 import os
-os.system('python quick_faucet_installer.py')
-os.system('python docker_installer.py')
-os.system('python poseidon_installation.py')
+os.system('python ./quick_faucet_installer.py')
+os.system('python ./docker_installer.py')
+os.system('python ./poseidon_installation.py')

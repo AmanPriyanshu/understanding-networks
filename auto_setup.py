c005415484ae00309bb40ec38f41ad594b47ac40
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-02-28 13:59:24 +0530

    Improving readability

--- auto_setup.py
+++ auto_setup.py
@@ -1,4 +1,4 @@
 import os
-os.system('python ./new1/quick_faucet_installer.py')
-os.system('python ./new1/docker_installer.py')
-os.system('python ./new1/poseidon_installation.py')
+os.system('python ./installers/quick_faucet_installer.py')
+os.system('python ./installers/docker_installer.py')
+os.system('python ./installers/poseidon_installation.py')

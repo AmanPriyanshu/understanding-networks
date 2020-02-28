618f7b49a9c24d8b00991f0dd7cf66e6c430881c
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2020-02-28 11:00:36 +0530

    Update poseidon_installation.py

--- poseidon_installation.py
+++ poseidon_installation.py
@@ -2,3 +2,4 @@ import os
 def poseidon_installation():
   os.system('curl -L https://raw.githubusercontent.com/CyberReboot/poseidon/master/bin/poseidon -o /usr/local/bin/poseidon')
   os.system('chmod +x /usr/local/bin/poseidon')
+poseidon_installation()

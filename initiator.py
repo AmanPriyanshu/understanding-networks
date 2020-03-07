2cbaac3ba5669b825974529a142c61e87478e394
AmanPriyanshu <aman.priyanshu@learner.manipal.edu>
2020-03-08 01:08:26 +0530

    Restarting from scratch

--- initiator.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('sudo ovs-vsctl add-br ovs-br1')
-os.system('sudo ovs-vsctl set bridge ovs-br1 fail_mode=secure')
-os.system('sudo ifconfig ovs-br1 173.16.1.1 netmask 255.255.255.0 up')
-os.system('sudo ovs-vsctl set Bridge ovs-br1 protocols=OpenFlow13')
-os.system('sudo ovs-vsctl set-controller ovs-br1 tcp:127.0.0.1:6653')
-os.system('sudo ovs-ofctl show -O OpenFlow13 ovs-br1')
